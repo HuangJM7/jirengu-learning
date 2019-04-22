8e04048652166650d80edebafb3d65fd29a7c357
HuangJM7 <jm_william@qq.com>
2019-04-22 21:04:46 +0800

    自动轮播防抖节流

--- 31进阶：把轮播改成无缝轮播吧/main.js
+++ 31进阶：把轮播改成无缝轮播吧/main.js
@@ -1,36 +1,44 @@
-var allImg = $('.wrap>img')
+let n, size, timerInterval, timerClick
+initialize()
+setInterval(() => {
+    if (!timerInterval) {
+        makeLeave(getImage(n))
+            .one('transitionend', (e) => {
+                makeEnter($(e.currentTarget))
+            })
+        makeCurrent(getImage(n + 1))
+        n += 1
+    }
+}, 3000)
 
-for (let i = 1; i < allImg.length; i++) {
-    $(allImg[i]).addClass('enter')
-}
-$('.wrap > img:nth-child(1)').addClass('current').removeClass('enter')
+right.addEventListener('click', function (e) {
+    if (!timerClick) {
+        makeLeave(getImage(n))
+            .one('transitionend', (e) => {
+                makeEnter($(e.currentTarget))
+            })
+        makeCurrent(getImage(n + 1))
+        n += 1
+
+        clearTimeout(timerInterval)//自动轮播防抖
+        timerInterval = setTimeout(() => {
+            timerInterval = null
+        }, 3000);
+
+        timerClick = setTimeout(() => {//节流
+            timerClick = null;
+        }, 1500);
+    }
 
 
-let n = 1
-let size = $('.wrap>img').length
 
-right.addEventListener('click', function () {
-    $(`.wrap > img:nth-child(${setN(n)})`)
-        .removeClass('current').addClass('leave')
-        .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $(`.wrap > img:nth-child(${setN(n+1)})`)
-        .removeClass('enter').addClass('current')
-    n += 1
 })
 
-setInterval(() => {
-    console.log(n);
-    $(`.wrap > img:nth-child(${setN(n)})`)
-        .removeClass('current').addClass('leave')
-        .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $(`.wrap > img:nth-child(${setN(n+1)})`)
-        .removeClass('enter').addClass('current')
-    n += 1
-}, 3000)
+
+
+function getImage(n) {
+    return $(`.wrap > img:nth-child(${setN(n)})`)
+}
 
 function setN(n) {
     if (n > size) {
@@ -38,6 +46,25 @@ function setN(n) {
         if (n === 0) {
             n = size
         }
-    } // n = 1 2 3
+    }
     return n
+}
+
+function initialize() {
+    n = 1
+    size = $('.wrap>img').length
+    $(`.wrap > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
 }
---
+++ 31进阶：把轮播改成无缝轮播吧/mainOld.js
@@ -0,0 +1,42 @@
+var allImg = $('.wrap>img')
+
+for (let i = 1; i < allImg.length; i++) {
+    $(allImg[i]).addClass('enter')
+}
+$('.wrap > img:nth-child(1)').addClass('current').removeClass('enter')
+
+let n = 1
+let size = $('.wrap>img').length
+
+right.addEventListener('click', function () {
+    $(`.wrap > img:nth-child(${setN(n)})`)
+        .removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $(`.wrap > img:nth-child(${setN(n+1)})`)
+        .removeClass('enter').addClass('current')
+    n += 1
+})
+
+setInterval(() => {
+    console.log(n);
+    $(`.wrap > img:nth-child(${setN(n)})`)
+        .removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $(`.wrap > img:nth-child(${setN(n+1)})`)
+        .removeClass('enter').addClass('current')
+    n += 1
+}, 3000)
+
+function setN(n) {
+    if (n > size) {
+        n = n % size
+        if (n === 0) {
+            n = size
+        }
+    } // n = 1 2 3
+    return n
+}
