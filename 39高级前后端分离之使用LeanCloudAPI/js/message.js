93bb7510b8ddd82cca372880fdd75bd3c2d7a689
HuangJM7 <jm_william@qq.com>
2019-05-04 12:37:16 +0800

    MVC message

--- 39高级前后端分离之使用LeanCloudAPI/js/message.js
+++ 39高级前后端分离之使用LeanCloudAPI/js/message.js
@@ -1,48 +1,82 @@
-var APP_ID = 's2Y9liqQkomyL7qUPQHIH0Jr-gzGzoHsz';
-var APP_KEY = 'gfgn7EMprjYIumLOtHTRv7X9';
+! function () {
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-var view = document.querySelector('#postMessageForm')
+    var view = document.querySelector('section.message')
 
-var query = new AV.Query('Message');
-updateMessage()
+    var model = {
+        // 获取数据
+        init: function () {
+            var APP_ID = 's2Y9liqQkomyL7qUPQHIH0Jr-gzGzoHsz';
+            var APP_KEY = 'gfgn7EMprjYIumLOtHTRv7X9';
+            AV.init({
+                appId: APP_ID,
+                appKey: APP_KEY
+            });
+        },
+        fetch: function () {
+            var query = new AV.Query('Message');
+            return query.find() // Promise 对象
+        },
+        // 创建数据
+        save: function (name, content) {
+            var Message = AV.Object.extend('Message');
+            var message = new Message();
+            return message.save({ // Promise 对象
+                'name': name,
+                'content': content
+            })
+        }
+    }
 
 
+    var controller = {
+        view: null,
+        model: null,
+        messageList: null,
+        init: function (view, model) {
+            this.view = view
+            this.model = model
 
-function updateMessage() {
-    messageList.innerHTML = ""
-    query.find().then(function (Message) {
-        Message.forEach(element => {
-            let li = document.createElement('li')
-            li.innerText = element.attributes.name + ':' + element.attributes.content
-            let messageList = document.querySelector('#messageList')
-            messageList.append(li)
-        });
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('form')
+            this.model.init()
+            this.loadMessages()
+            this.bindEvents()
+        },
+        loadMessages: function () {
+            this.model.fetch().then(
+                (messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.messageList.appendChild(li)
+                    })
+                }
+            )
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
+            })
+        },
+        saveMessage: function () {
+            let myForm = this.form
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            this.model.save(name, content).then(function (object) {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                myForm.querySelector('input[name=content]').value = ''
+                console.log(object)
+            })
+        }
 
-    })
-}
+    }
+
+    controller.init(view, model)
 
 
-view.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let content = view.querySelector('input[name=content]').value
-    let name = view.querySelector('input[name=name]').value
-    var Message = AV.Object.extend('Message')
-    var message = new Message()
-    message.save({
-        'content': content,
-        'name': name,
-    }).then(function (object) {
-        console.log('post');
-        console.log(object);
-        let li = document.createElement('li')
-        li.innerText = name + ':' + content
-        let messageList = document.querySelector('#messageList')
-        messageList.append(li)
-        view.querySelector('input[name=content]').value = ""
-
-    })
-})
+}.call()
